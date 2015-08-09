f60acf0a00d711f2eea355a010098ca89f67c57f
chizik <sagi.chizik@gmail.com>
2015-08-09 11:59:25 +0300

    panel init

--- src/app/feedbacks/feedbacksPanel.js
+++ src/app/feedbacks/feedbacksPanel.js
@@ -1,4 +1,4 @@
-define(['react', 'lodash', './feedbacks.rt'], function (React, _, template) {
+define(['react', 'lodash', './feedbacksPanel.rt'], function (React, _, template) {
     'use strict';
 
     return React.createClass({
