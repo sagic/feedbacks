78df3b8bfd65e9f3da4ac3bcf0019c1831ba8612
chizik <sagi.chizik@gmail.com>
2015-08-09 11:59:57 +0300

    list and avatar init

---
+++ src/app/components/userAvatar.js
@@ -0,0 +1,8 @@
+define(['react', 'lodash', './userAvatar.rt'], function (React, _, template) {
+    'use strict';
+
+    return React.createClass({
+        displayName: 'userAvatar',
+        render: template
+    });
+});
---
+++ src/app/feedbacks/feedbacksList.js
@@ -0,0 +1,8 @@
+define(['react', 'lodash', './feedbacksList.rt'], function (React, _, template) {
+    'use strict';
+
+    return React.createClass({
+        displayName: 'feedbacksList',
+        render: template
+    });
+});
